30002d00c99c528856cef25a2ba6c9575b436425
Tomas Surovec <tomas.surovec87@gmail.com>
2017-04-04 20:39:27 +0200

    Go - line analyze complete

--- go2/main.go
+++ go2/main.go
@@ -5,6 +5,7 @@ import (
 	"bufio"
 	"os"; "regexp"
 	"encoding/hex"
+	"encoding/binary"
 )
 
 type dataLine struct {
@@ -25,7 +26,10 @@ func main() {
 	for ok == nil {
 		line, ok = reader.ReadString('\n')
 		fmt.Printf("%v", line)
-		analyze(line)
+		a := analyze(line)
+		if a.loaded {
+			fmt.Printf("Offset = %v; Data: %v\n", a.offset, a.data)
+		}
 	}	
 	
 	fmt.Printf("END %v", ok)
@@ -33,35 +37,35 @@ func main() {
 
 func analyze(line string) dataLine {
 	regex, _ := regexp.Compile("^\\s*0x([0-9a-fA-F]{4}):\\s+(.*)\\s*$")
-//	regex, _ := regexp.Compile("^\\s*0x([0-9a-fA-F]{4}):\\s+([0-9a-fA-F]+\\s+)+\\s*$")
-
-	sms := regex.FindStringSubmatch(line)
-	if len(sms) == 0 {
+	
+	dataLineMatch := regex.FindStringSubmatch(line)
+	if len(dataLineMatch) == 0 {
 		return dataLine{loaded: false}
 	}
-
-	// todo: sms[1] to uint16
 	
-	dl := dataLine{loaded: true, offset: /*sms[1]*/0}
-
-
-	//var dst [20]byte 
-	sms1dec, _ := hex.DecodeString(sms[1])
-
-
+	offsetBytes, _ := hex.DecodeString(dataLineMatch[1])
+	dl := dataLine{loaded: true, offset: binary.BigEndian.Uint16(offsetBytes)}
 	
-	var data string
-	data = sms[2]
+	data := dataLineMatch[2]
 
-	fmt.Printf("__%v__; %v, %v\n", sms[1], len(sms1dec),sms1dec)
+	dataRegex, _ := regexp.Compile("^\\s*([0-9a-fA-F]{2})(.*)$")
 
-	// todo: skip whitespaces, read 2 bytes, if hexabytes, convert to byte, skip, continue until end of string
-	// or bad character encountered
-	fmt.Printf("DATA=%v\n", data)
+	var tgt []byte
+	nonempry := true
+	for nonempry {
+		onebyte := dataRegex.FindStringSubmatch(data)
+		if len(onebyte) > 0 {
+			//fmt.Printf("\t1: %v ; %v\n", onebyte[1], len(onebyte))
+			deko, _ := hex.DecodeString(onebyte[1])
+			tgt = append(tgt, deko[0])
+			if(len(onebyte) > 2) {
+				data = onebyte[2]
+			}			
+		} else {
+			nonempry=false
+		}
+	}
 
-	kkt, _ := hex.DecodeString(data[:4])
-	fmt.Printf("%v, %v, %v\n",data[:4], len(kkt),kkt)
-	
-	
+	dl.data = tgt	
 	return dl
 }
