35574168195d3ea9751d27e97479668967866a1e
Tomas Surovec <tomas.surovec87@gmail.com>
2017-04-06 15:12:36 +0200

    more system

--- go2/main.go
+++ go2/main.go
@@ -16,14 +16,90 @@ type dataLine struct {
 }
 
 type layer interface {
+	// loads from data, returns this and number of bytes loaded
 	load([]byte) (layer, uint16)
 	getName() string
-	getColor() string
+	next_layer_hint() int
+	get_lines() []string
+	set_color(string) layer
+ 	get_color() string
+}
+
+const (
+	COLOR_NORMAL = "\x1B[0m"
+	COLOR_RED = "\x1B[31m"
+	COLOR_GREEN = "\x1B[32m"
+	COLOR_YELLOW = "\x1B[33m"
+	COLOR_BLUE = "\x1B[34m"
+	COLOR_MAGENTA = "\x1B[35m"
+	COLOR_CYAN = "\x1B[36m"
+	COLOR_WHITE = "\x1B[37m"
+)
+
+type unknown_layer struct {color string}
+func (this unknown_layer)load(data []byte) (layer, uint16) {
+	return this, uint16(len(data))
+}
+func (this unknown_layer)getName() string {
+	return "UNKNOWN"
+}
+func (this unknown_layer)get_lines() []string  {
+	return []string { "-- no interpretation --" }
+}
+func (this unknown_layer)next_layer_hint() int {
+	return UNKNOWN
+}
+func (this unknown_layer) set_color(color string) layer {
+	this.color = color
+	return this
+}
+func (this unknown_layer)get_color() string {
+	return this.color
+}
+
+func (this ethernet_frame) get_lines() []string {
+	return []string {
+		fmt.Sprintf("Destionation MAC = %x", this.mac_dest),
+		fmt.Sprintf("Source MAC = %x", this.mac_src),
+		fmt.Sprintf("Ethertype = 0x%04x", this.ethertype),
+	}
+}
+func(this ethernet_frame) set_color(clr string) layer {
+	this.color = clr
+	return this
+}
+func(this ethernet_frame) get_color() string {
+	return this.color
+}
+func (this ethernet_frame) load(data []byte) (layer, uint16) {
+	// todo: assert(len(data) >= 14)
 	
-	// get fields/print
+	for i := 0 ; i < 6; i++ {
+		this.mac_dest[i] = data[i]
+	}
+	for i := 0 ; i < 6; i++ {
+		this.mac_src[i] = data[6 + i]
+	}
+	
+	this.ethertype = binary.BigEndian.Uint16(data[12:14])	
+	
+	return this, 14 
+}
+
+func (this ethernet_frame) getName() string {
+	return "Ethernet"
+}
+func (this ethernet_frame)next_layer_hint() int {
+	if this.ethertype == 0x800 {
+		return IP
+	}
+//todo: if ethertype == 0x800, then ip; 806->arp,etc
+	return UNKNOWN
 }
 
 type ethernet_frame struct {
+	color string
+	
 	mac_dest [6]byte
 	mac_src [6]byte
 	ethertype uint16
@@ -31,7 +107,8 @@ type ethernet_frame struct {
 	// 32 bits frame check seq
 }
 
-type ip_header struct {
+type ip_packet struct {
+	// header
 	version_ihl uint8;
 	dscp_ecn uint8;
 	total_length uint16;
@@ -41,12 +118,69 @@ type ip_header struct {
 	protocol uint8;
 	header_checksum uint16;
 	ip_src uint32;
-	ip_dst uint32;	
+	ip_dst uint32;
+
+	// other
+	color string
 }
+
+func (this ip_packet)load(data []byte) (layer, uint16) {
+	this.version_ihl = uint8(data[0])
+	this.dscp_ecn = uint8(data[1])
+	
+	return this, uint16((this.version_ihl & 0xf) << 2)
+}
+func (this ip_packet)getName() string {
+	return "IP"
+}
+func (this ip_packet)next_layer_hint() int {
+	return UNKNOWN
+}
+func (this ip_packet)get_lines() []string {
+	return []string {
+		fmt.Sprintf("Version: %d", this.version_ihl >> 4),
+		fmt.Sprintf("IHL: %d", this.version_ihl & 0x0f),
+	}
+}
+func (this ip_packet)set_color(color string) layer {
+	this.color = color
+	return this
+}
+func (this ip_packet)get_color() string {
+	return this.color
+}
+
+
 
 type colored_data struct {
 	offset uint16
 	color string
+}
+
+const (
+	ETHERNET = iota
+	IP = iota
+	ARP = iota
+	TCP = iota
+	HTTP = iota
+	TLS_RECORD = iota
+	UNKNOWN = iota
+)
+
+func load_layer(type_hint int, data []byte) (layer, uint16) {
+	var r layer
+	var bytes_loaded uint16
+	switch type_hint {
+	case ETHERNET:
+		r = ethernet_frame{}
+	case IP:
+		r = ip_packet{}
+	default:
+		r = unknown_layer{} 
+	}
+	
+	r, bytes_loaded = r.load(data)
+	return r, bytes_loaded	
 }
 
 // todo: groupBy N bytes, perLine N bytes
@@ -63,8 +197,6 @@ func printColored(boundaries []colored_data, data []byte) []string {
 	kolo := "\x1B[0m"
 	
 	for line := 0; line < nlines; line++ {
-//		line_data := data[per_line * line:per_line]
-
 		s := "\t0x"
 		xy := make([]byte, 2)
 		line_offset := line * per_line
@@ -112,8 +244,16 @@ func printColored(boundaries []colored_data, data []byte) []string {
 }
 
 func main() {
+	colors := []string {COLOR_RED,
+		COLOR_GREEN,
+		COLOR_YELLOW,
+		COLOR_BLUE,
+		COLOR_MAGENTA,
+		COLOR_CYAN}
+	
 	fmt.Println("PAGO \x1B[32m>\x1B[0m")
-	coloring := []colored_data{ colored_data{offset: 0, color: "\x1B[32m"},
+	coloring := []colored_data{
+		colored_data{offset: 0, color: "\x1B[32m"},
 		colored_data{offset: 14, color: "\x1B[33m"},
 		colored_data{offset: 34, color: "\x1B[36m"},
 		colored_data{offset: 54, color: "\x1B[35m"},
@@ -140,25 +280,46 @@ func main() {
 
 
 		if (!a.loaded && ll) || (a.loaded && len(a.data) < 16) {
+			current_color := colors[0]
+			coloring = []colored_data{colored_data{offset: 0, color: current_color}}
+			i := 1
+
+			layers := []layer{}
+			
+			// interpretation
+			offset := 0
+			var current_layer layer
+			var bytes_loaded uint16
+			layer_hint := ETHERNET
+			for offset < len(packet) {
+				current_layer, bytes_loaded = load_layer(layer_hint, packet[offset:])
+				layer_hint = current_layer.next_layer_hint()
+				offset = offset + int(bytes_loaded)
+				
+				current_layer = current_layer.set_color(current_color)
+				
+				coloring = append(coloring, colored_data{offset: uint16(offset), color:
+					colors[i]})
+				current_color = colors[i]
+				layers = append(layers, current_layer)
+				i++
+			} 
+
+			// Colored lines with data
 			clines := printColored(coloring, packet)
 			for i := 0; i< len(clines); i++ {
 				fmt.Println(clines[i])
 			}
+
+			// Textual lines
+			for i = 0; i < len(layers); i++ {
+				fmt.Printf("Layer %s%s%s:\n", layers[i].get_color(), layers[i].getName(), COLOR_NORMAL)
+				for j := 0; j < len(layers[i].get_lines()); j++ {
+					fmt.Printf("\t%s\n", layers[i].get_lines()[j])
+				} 
+			}
 			
-			ll = false
-			fmt.Printf("Interpreting packet, len = %v\n", len(packet))
-			// todo: interpretation
-
-			ef, r := get_l2(packet)
-			fmt.Printf("EtherType = %v; Rest (L3): %v bytes\n", ef.ethertype, len(r))
-
-			iph, r2 := get_l3(ef.ethertype, r)
-			fmt.Printf("%v.%v.%v.%v; Rest (L4): %v bytes\n", iph.ip_src >> 24,
-				0xff&(iph.ip_src >> 16),
-				(iph.ip_src >> 8)&0xff,
-				iph.ip_src & 0xff, len(r2))
-			
-
+			ll = false			
 			packet = packet[:0]
 			data_lines = data_lines[:0]
 		}
@@ -172,7 +333,7 @@ func main() {
 	
 	fmt.Printf("END %v\n\n", ok)
 
-	var x ip_header
+	var x ip_packet
 	sf := reflect.Type.Field(reflect.TypeOf(x), 0)
 	fmt.Println(sf)
 }
@@ -191,7 +352,7 @@ func get_l2(data []byte) (ethernet_frame, []byte) {
 	return ef, data[14:]
 }
 
-func get_l3(ethertype uint16, data []byte) (ip_header, []byte) {
+/*func get_l3(ethertype uint16, data []byte) (ip_header, []byte) {
 	ip := ip_header{}
 
 	ip.version_ihl = data[0]
@@ -200,18 +361,18 @@ func get_l3(ethertype uint16, data []byte) (ip_header, []byte) {
 	// etc.
 	ip.ip_src = binary.BigEndian.Uint32(data[12:16])
 	ip.ip_dst = binary.BigEndian.Uint32(data[16:20])
-/*	for i := 0 ; i < 6; i++ {
-		ef.mac_dest[i] = data[i]
-	}
-	for i := 0 ; i < 6; i++ {
-		ef.mac_src[i] = data[6 + i]
-	}
-	ef.ethertype = binary.BigEndian.Uint16(data[12:14])	
-	return ef, data[14:]*/
+//	for i := 0 ; i < 6; i++ {
+//		ef.mac_dest[i] = data[i]
+//	}
+//	for i := 0 ; i < 6; i++ {
+//		ef.mac_src[i] = data[6 + i]
+//	}
+//	ef.ethertype = binary.BigEndian.Uint16(data[12:14])	
+//	return ef, data[14:]
 
 	l4offset := 20 // default ip header length
 	return ip, data[l4offset:]
-}
+}*/
 
 func analyze(line string) dataLine {
 	regex, _ := regexp.Compile("^\\s*0x([0-9a-fA-F]{4}):\\s+(.*)\\s*$")
